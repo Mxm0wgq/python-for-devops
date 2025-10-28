6eeb8b98cc5073b2353417e87eacfc946900f033
Mxm0wgq <159918522+Mxm0wgq@users.noreply.github.com>
2025-10-28 15:53:07 +0000

    My-Python-Work

---
+++ My-Project-Datatypes/No.py
@@ -0,0 +1,12 @@
+ x=2
+ y=2.4
+print(abs(x))
+print (abs(-x))
+print(abs(y))
+print(round(x))
+print(round(2 , 5))
+print(round(2.675 , 2))
+print(pow(x , y))
+print(pow(3 , 4 , 5))
+print(min(x ,y))
+print(max(x , y))
---
+++ My-Project-Datatypes/Str.py
@@ -0,0 +1,15 @@
+print("Hello-world")
+print ("welcome to My-Project!")
+str1="Let's start /coding."
+str2="Have a great day!"
+print(str1+" "+str2)
+print(str1.split('/')[1])
+print(len(str1))
+print(str1.replace("start","for"))
+print(str1.upper())
+print(str2.lower())
+print(str1.join(str2))
+print(str1.capitalize())
+print(str2.startswith("hello"))
+print(str2.endswith("rrrr"))
+
